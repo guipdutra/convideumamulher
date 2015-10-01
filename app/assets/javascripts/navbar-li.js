e5f7f13fa9e697d69db87ca571bbd3d2cad0c37c
Julio Farah <julio.farah@gmail.com>
2015-09-30 22:06:40 -0300

    remove navbar js

--- app/assets/javascripts/navbar-li.js
+++
@@ -1,8 +0,0 @@
-$('.nav nav-tabs li').click(function(e) {
-  $('.navbar li.active').removeClass('active');
-  var $this = $(this);
-  if (!$this.hasClass('active')) {
-    $this.addClass('active');
-  }
-  e.preventDefault();
-});
